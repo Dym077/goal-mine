d0df321ccf81f6752bb05405ae415028c1f73006
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-19 12:45:46 +0000

    Add secret key

--- goalmine/settings.py
+++ goalmine/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zw9h(n_f%f8-5uk_o-*hg#yoo+w6^yy6hgyn%et8w!4s_yyaq%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
