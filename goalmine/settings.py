6c1235373ef6c9972f94476e5c2a883b97b5a337
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-19 14:40:38 +0000

    Add a Goal model

--- goalmine/settings.py
+++ goalmine/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-dym077-goalmine-bl6hqnt87ct.ws-eu110.gitpod.io', '.herokuapp.com']
 
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'goalmine.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
