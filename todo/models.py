3728f63b511b2917ab2451b582effe1413158de9
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-21 08:35:20 +0000

    Add Task model

--- todo/models.py
+++ todo/models.py
@@ -16,3 +16,14 @@ class Goal(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+class Task(models.Model):
+    goal = models.ForeignKey(Goal,)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasks"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    
