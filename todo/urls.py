03ac185034cc62fdd218cbefdb60b89987e28133
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-23 22:40:29 +0000

    Add goal_detail view to templates

--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GoalsList.as_view(), name='home'),
-    path('<slug:slug>/',
-    views.goal_detail, name='goal_detail'),
+    path('<slug:slug>/', views.goal_detail, name='goal_detail'),
    
 ]
