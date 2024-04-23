8ae2258a7fc74da4a116e461bfd99074c2c1ffdf
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-23 17:05:01 +0000

    Additional html code for base and index-pages

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GoalsList.as_view(), name='home'),
+    path('<slug:slug>/',
+    views.goal_detail, name='goal_detail'),
+   
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Goal, Task
 
@@ -8,3 +8,11 @@ class GoalsList(generic.ListView):
     queryset = Goal.objects.filter(status=1)
     template_name = "todo/index.html"
     paginate_by = 6
+
+def goal_detail(request, slug):
+    queryset = Goal.objects.filter(status=1)
+    goal = get_object_or_404(queryset, slug=slug)
+    return render(request, "todo/goal_detail.html", {"goal": goal},) 
+
+
+    
