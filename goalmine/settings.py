0478673558485def2904ace7201b955d1b1ab467
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-23 07:32:23 +0000

    Enable serving of static files

--- goalmine/settings.py
+++ goalmine/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.meddleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
