80febe3a11ce17b25ce008428383a53481976d5f
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-26 15:01:38 +0000

    Add visibility for tasks on goal detail page

--- manager/models.py
+++ manager/models.py
@@ -5,8 +5,8 @@ from todo.models import Goal
 # Create your models here.
 
 class Task(models.Model):
-    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name="tasks", null=True, blank=True) 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author", null=True, blank=True)
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name="tasks") 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -16,4 +16,8 @@ class Task(models.Model):
         
     def __str__(self):
         return f"Task {self.body} | added by {self.author}"    
+
+    def profile_page(request):
+        user = get_object_or_404(User, user=request.user)
+        comments = user.author.all()    
 
