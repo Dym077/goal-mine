dd0c07dd51834d7dd948d1127c5c0a1a8a264a66
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-26 11:39:06 +0000

    Make tasks visible on separate goals

--- manager/models.py
+++ manager/models.py
@@ -5,8 +5,8 @@ from todo.models import Goal
 # Create your models here.
 
 class Task(models.Model):
-    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name="tasks") 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name="tasks", null=True, blank=True) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author", null=True, blank=True)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
--- todo/views.py
+++ todo/views.py
@@ -13,7 +13,8 @@ def goal_detail(request, slug):
     queryset = Goal.objects.filter(status=1)
     goal = get_object_or_404(queryset, slug=slug)
     tasks = goal.tasks.all().order_by("-created_on")
-    goal_count = goal.tasks.filter(approved=True).count()
+    print(tasks)
+    task_count = len(tasks)
 
     return render(request, "todo/goal_detail.html", {"goal": goal, "tasks": tasks, "task_count": task_count},) 
 
